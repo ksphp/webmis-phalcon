8f1c524191e70e7c3b25faefeb7f768f7c894f8b
kingsoul <admin@ksphp.com>
2015-09-08 17:18:18 +0800

    Updata WebMIS Plugin

--- public/themes/admin/default/login.js
+++ public/themes/admin/default/login.js
@@ -3,9 +3,34 @@ $(function(){
 	$('#webmisVersion').webmisVersion();
 	// Auto Size
 	var autoSize = function(){
-		var height = ($(window).height()-$('#webmisVersion').height()-$('.login_body').height()-$('.login_copy').height())/3;
+		var height = ($(window).height()-300)/2;
 		$('#webmisVersion').css({'margin-top':height});
 	}
 	autoSize();
 	$(window).resize(function(){autoSize();});
+	
+	// Login
+	var login = function(){
+		var uname = $('#uname').val();
+		var passwd = $('#passwd').val();
+		var is_mobile = $('#is_mobile').text();
+		if(uname.length < 1 || passwd.length < 1){
+			$.webmis.win('open',{content:'<b class="red">帐号或密码为空！</b>',AutoClose:3});
+			return false;
+		}else{
+			$.post($base_url+'index/login',{'uname':uname,'passwd':passwd,'is_mobile':is_mobile},function(data){
+				if(data.status == 'y'){
+					$.webmis.win('close','welcome');
+				}else{
+					$.webmis.win('open',{content:'<b class="red">'+data.msg+'</b>',AutoClose:3});
+				}
+				return false;
+			},"json");
+		}
+		return false;
+	}
+	// Enter Login
+	$(document).keypress(function(e){if(e.which == 13){login();}});
+	// Click Login
+	$('#adminLogin').click(login);
 });
