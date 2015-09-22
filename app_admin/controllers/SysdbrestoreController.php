11cb60b06f3890e5327167068bb3745390827098
root <admin@ksphp.com>
2015-09-22 17:31:07 +0800

    DB Restore

--- app_admin/controllers/SysdbrestoreController.php
+++ app_admin/controllers/SysdbrestoreController.php
@@ -2,10 +2,16 @@
 class SysDBRestoreController extends ControllerBase{
 	// Index
 	public function indexAction(){
-		$Menus = $this->inc->getMenus();
+		// Menus
+		$this->view->setVar('Menus',$this->inc->getMenus());
 		$this->tag->prependTitle($this->inc->Ctitle);
-		$this->view->setVar('Menus',$Menus);
+		// Data
+		$data = '';
+		$this->view->setVar('Tables',$data);
+		$this->view->setVar('Lang',$this->inc->getLang('system/sys_db'));
+		$this->view->setVar('LoadJS', array('system/sys_db_backup.js'));
+		// View
 		$this->view->setTemplateAfter(APP_THEMES.'/main');
-		$this->view->pick("system/db/restore/index");
+		// $this->view->pick("system/db/restore/index");
 	}
 }
