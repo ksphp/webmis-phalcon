909567d18ae6026a977689417262daf727fd61e8
root <admin@ksphp.com>
2015-11-14 17:27:04 +0800

    Desktop

--- app_admin/controllers/DesktopController.php
+++ app_admin/controllers/DesktopController.php
@@ -2,10 +2,43 @@
 class DesktopController extends ControllerBase{
 	// Index
 	public function indexAction(){
-		$Menus = $this->inc->getMenus();
+		// Data
+		//$this->view->setVar('Lang',$this->inc->getLang('welcome/desktop'));
+		$this->view->setVar('LoadJS', array('welcome/desktop.js'));
+		// Menus
+		$this->view->setVar('Menus',$this->inc->getMenus());
 		$this->tag->prependTitle($this->inc->Ctitle);
-		$this->view->setVar('Menus',$Menus);
+		// View
 		$this->view->setTemplateAfter(APP_THEMES.'/main');
 		$this->view->pick("welcome/desktop");
+	}
+	/* Chart */
+	public function chartAction(){
+		$data = array(
+			'labels'=>array('January','February','March','April','May','June','July','August','September','October','November','December'),
+			'datasets'=>array(
+				array(
+					'label'=>'My First dataset',
+					'fillColor'=>'rgba(220,220,220,0.2)',
+					'strokeColor'=>'rgba(220,220,220,1)',
+					'pointColor'=>'rgba(220,220,220,1)',
+					'pointStrokeColor'=>'#fff',
+					'pointHighlightFill'=>'#fff',
+					'pointHighlightStroke'=>'rgba(220,220,220,1)',
+					'data'=>array(0, 0, 0, 0, 0, 65, 59, 80, 81, 56, 55, 40)
+				),
+				array(
+					'label'=>'My Second dataset',
+					'fillColor'=>'rgba(151,187,205,0.2)',
+					'strokeColor'=>'rgba(151,187,205,1)',
+					'pointColor'=>'rgba(151,187,205,1)',
+					'pointStrokeColor'=>'#fff',
+					'pointHighlightFill'=>'#fff',
+					'pointHighlightStroke'=>'rgba(151,187,205,1)',
+					'data'=>array(28, 48, 0, 19, 86, 27, 90)
+				)
+			)
+		);
+		return $this->response->setJsonContent($data);
 	}
 }
---
+++ public/themes/admin/js/welcome/desktop.js
@@ -0,0 +1,35 @@
+$(function(){
+	$.webmis.inc({files:[$webmis_plugin+'chart/Chart.min.js']});
+	$.post($base_url+'Desktop/chart',function(data){
+		// 创建图表
+		var ctx = $("#myChart").get(0).getContext("2d");
+		var myNewChart = new Chart(ctx).Line(data);
+	},'json');
+	// 数据
+	var data = {
+		labels: ["January", "February", "March", "April", "May", "June", "July"],
+		datasets: [
+			{
+				label: "My First dataset",
+				fillColor: "rgba(220,220,220,0.2)",
+				strokeColor: "rgba(220,220,220,1)",
+				pointColor: "rgba(220,220,220,1)",
+				pointStrokeColor: "#fff",
+				pointHighlightFill: "#fff",
+				pointHighlightStroke: "rgba(220,220,220,1)",
+				data: [65, 59, 80, 81, 56, 55, 40]
+			},
+			{
+				label: "My Second dataset",
+				fillColor: "rgba(151,187,205,0.2)",
+				strokeColor: "rgba(151,187,205,1)",
+				pointColor: "rgba(151,187,205,1)",
+				pointStrokeColor: "#fff",
+				pointHighlightFill: "#fff",
+				pointHighlightStroke: "rgba(151,187,205,1)",
+				data: [28, 48, 40, 19, 86, 27, 90]
+			}
+		]
+	};
+	
+});
