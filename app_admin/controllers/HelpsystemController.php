0b01c6a180576960093f3f0d50c183c84882327a
root <admin@ksphp.com>
2015-11-15 17:46:00 +0800

    M Log

--- app_admin/controllers/HelpsystemController.php
+++ app_admin/controllers/HelpsystemController.php
@@ -6,7 +6,12 @@ class HelpSystemController extends ControllerBase{
 		$this->view->setVar('Menus',$this->inc->getMenus());
 		$this->tag->prependTitle($this->inc->Ctitle);
 		// View
-		$this->view->setTemplateAfter(APP_THEMES.'/main');
-		$this->view->pick("help/system/index");
+		if($this->session->get('IsMobile')){
+			$this->view->setTemplateAfter(APP_THEMES.'/main_m');
+			$this->view->pick("help/system/index_m");
+		}else{
+			$this->view->setTemplateAfter(APP_THEMES.'/main');
+			$this->view->pick("help/system/index");
+		}
 	}
 }
