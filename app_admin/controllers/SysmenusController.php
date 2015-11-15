7a50dca7af1b06bd48488890d37eff86169bb49b
root <admin@ksphp.com>
2015-11-15 17:30:18 +0800

    M Sys Menus

--- app_admin/controllers/SysmenusController.php
+++ app_admin/controllers/SysmenusController.php
@@ -26,8 +26,14 @@ class SysMenusController extends ControllerBase{
 		$this->view->setVar('Menus',$this->inc->getMenus());
 		$this->tag->prependTitle($this->inc->Ctitle);
 		// View
-		$this->view->setTemplateAfter(APP_THEMES.'/main');
-		$this->view->pick("system/menus/index");
+		if($this->session->get('IsMobile')){
+			$this->view->setTemplateAfter(APP_THEMES.'/main_m');
+			$this->view->pick("welcome/sys_change_passwd_m");
+			$this->view->pick("system/menus/index_m");
+		}else{
+			$this->view->setTemplateAfter(APP_THEMES.'/main');
+			$this->view->pick("system/menus/index");
+		}
 	}
 	
 	/* Search */
