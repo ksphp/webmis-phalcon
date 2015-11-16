36c523ed6b0d1c9a42a02389efe43ff8d5611e70
root <admin@ksphp.com>
2015-11-16 10:02:10 +0800

    M SysFilemanager

--- app_admin/controllers/SysfilemanagerController.php
+++ app_admin/controllers/SysfilemanagerController.php
@@ -17,7 +17,12 @@ class SysFilemanagerController extends ControllerBase{
 		$this->view->setVar('Menus',$this->inc->getMenus());
 		$this->tag->prependTitle($this->inc->Ctitle);
 		// View
-		$this->view->setTemplateAfter(APP_THEMES.'/main');
-		$this->view->pick("system/filemanager/index");
+		if($this->session->get('IsMobile')){
+			$this->view->setTemplateAfter(APP_THEMES.'/main_m');
+			$this->view->pick("system/filemanager/index_m");
+		}else{
+			$this->view->setTemplateAfter(APP_THEMES.'/main');
+			$this->view->pick("system/filemanager/index");
+		}
 	}
 }
