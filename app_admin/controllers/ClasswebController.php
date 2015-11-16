8f7975eb884d77e282c66748db764b4571ebe495
root <admin@ksphp.com>
2015-11-16 10:18:47 +0800

    M Web

--- app_admin/controllers/ClasswebController.php
+++ app_admin/controllers/ClasswebController.php
@@ -25,8 +25,13 @@ class ClasswebController extends ControllerBase{
 		$this->view->setVar('Menus',$this->inc->getMenus());
 		$this->tag->prependTitle($this->inc->Ctitle);
 		// View
-		$this->view->setTemplateAfter(APP_THEMES.'/main');
-		$this->view->pick("class/web/index");
+		if($this->session->get('IsMobile')){
+			$this->view->setTemplateAfter(APP_THEMES.'/main_m');
+			$this->view->pick("class/web/index_m");
+		}else{
+			$this->view->setTemplateAfter(APP_THEMES.'/main');
+			$this->view->pick("class/web/index");
+		}
 	}
 	/* Search */
 	public function seaAction(){
