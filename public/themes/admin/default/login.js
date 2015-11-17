103711ded12132237235e4d8fdb49521c262b8c3
root <admin@ksphp.com>
2015-11-17 21:37:31 +0800

    test

--- public/themes/admin/default/login.js
+++ public/themes/admin/default/login.js
@@ -25,7 +25,7 @@ $(function(){
 		var passwd = $('#passwd').val();
 		var is_mobile = $('#is_mobile').text();
 		if(uname.length < 1 || passwd.length < 1){
-			$.get($base_url+'index/getLang/msg',{msg_title:'',msg_isNull:'',msg_auto_close:''},function (data){
+			$.get($base_url+'Result/getLang/msg',{msg_title:'',msg_isNull:'',msg_auto_close:''},function (data){
 				$.webmis.win('open',{title:data.msg_title, content:'<b class="red">'+data.msg_isNull+'</b>',AutoClose:3,AutoCloseText:data.msg_auto_close});
 			},'json');
 			return false;
