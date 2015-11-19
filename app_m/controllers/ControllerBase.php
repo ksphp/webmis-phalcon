8f2561427739271157d9a9446a32eac2aa130785
root <admin@ksphp.com>
2015-11-19 15:44:56 +0800

    Web and M

--- app_m/controllers/ControllerBase.php
+++ app_m/controllers/ControllerBase.php
@@ -3,6 +3,9 @@ use Phalcon\Mvc\Controller;
 class ControllerBase extends Controller{
 	protected function initialize(){
 		$this->tag->setTitle(' | '.APP_TITLE);
+		// URL
+		$this->url->setStaticBaseUri($this->inc->BaseUrl());
+		$this->url->setBaseUri($this->inc->BaseUrl(APP_NAME));
 	}
 }
 
