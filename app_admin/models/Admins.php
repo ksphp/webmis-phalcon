04b1fc0b958ce5d09f17e560a64909e8ac806a48
root <admin@ksphp.com>
2016-01-13 20:52:27 +0800

    js

--- app_admin/models/Admins.php
+++ app_admin/models/Admins.php
@@ -2,13 +2,6 @@
 use Phalcon\Mvc\Model;
 class Admins extends Model{
 	public $id;
-	public $uname;
-	public $name;
-	public $department;
-	public $position;
-	public $email;
-	public $perm;
-	public $state;
 	public function getSource(){
 		return "wmis_sys_admin";
 	}
