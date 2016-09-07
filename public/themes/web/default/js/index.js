f235a8d28e84a18a11d6bc0903e43d311db3944a
kingsoul <admin@ksphp.com>
2016-09-07 13:30:49 +0800

    Information prompt

--- public/themes/web/default/js/index.js
+++ public/themes/web/default/js/index.js
@@ -1,8 +1,7 @@
 $(function (){
 	// Auto Size
 	var autoSize = function(){
-		var height = ($(window).height()-$('.in_body').height())/3;
-		$('.in_body').css({'margin-top':height});
+		var height = ($(window).height();
 	}
 	autoSize();
 	$(window).resize(function(){autoSize();});
