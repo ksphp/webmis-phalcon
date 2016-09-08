feacea26c6ee1360d34608b44450612f9c16e5ce
kingsoul <305549985@qq.com>
2016-09-08 02:22:22 -0500

    update install

--- public/install/index.php
+++ public/install/index.php
@@ -82,7 +82,7 @@ if (isset($_POST['install'])){
 	}
 	$passwd = $_POST['passwd'];
 	if(strlen($passwd)<6 || strlen($passwd)>32){
-		$msg = $Lang['passwd'].' 6~32 '.$Lang['msg_chr'];
+		$msg = strlen($passwd).$Lang['passwd'].' 6~32 '.$Lang['msg_chr'];
 	}
 	$type = $_POST['type'];
 	$hostname = trim($_POST['hostname']);
@@ -168,19 +168,19 @@ if (isset($_POST['install'])){
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
 <style type="text/css">
 body,div,span,ul,li,table,tr,td,input,a,h1,h2,h3,h4{margin: 0; padding: 0;}
-body{font-size: 12px; background: url("bg.png") #3D3D3D center top; color: #DFE1E8;}
+body{font-size: 12px; background: url("bg.png")  #2D2D2D center center; background-size: 0.8%; color: #DFE1E8;}
 a{color: #007568; text-decoration: none;}
 a:hover{color: #FF6600;}
 input{box-shadow: 0 1px 1px rgba(0, 0, 0, 0.075) inset;}
 input:focus{box-shadow: 0 0 6px rgba(153, 153, 153, 0.7);}
 .input{height: 30px; line-height: 30px; border: #999 1px solid; background: #FFF; display: inline-block; padding: 0 5px;}
 .copy{padding: 10px 0; text-align: center;}
-.lang{position: absolute; top: 10px; right: 15px; padding: 5px 20px; background-color: #333; color: #DFE1E8; border-radius: 5px;}
+.lang{position: absolute; top: 15px; right: 15px; padding: 8px 20px; background-color: #333; color: #DFE1E8; border-radius: 5px;}
 .lang a{color: #DFE1E8;}
 .lang a:hover{color: #FF6600;}
-h1{font-size: 62px; margin-top: 34px; text-align: center;}
+h1{position: absolute; width: 420px; font-size: 62px; color: #FFF; text-align: center; margin-top: -100px;}
 h2{font-size: 14px; border-bottom: #DADCDF 1px solid; color: #999;}
-.body{width: 380px; background-color: #F5F5F5; color: #666; border-radius: 5px; margin: 0 auto; padding: 10px 20px;}
+.body{position: absolute; width: 420px; left: 0; right: 0; top: 20%; margin: 0 auto; background-color: #F5F5F5; color: #666; border-radius: 5px; padding: 10px 20px;}
 .list{padding: 10px 0px; margin: 0;}
 .list dt{float: left; width: 80px; line-height: 32px; text-align: right; padding: 5px;}
 .list dd{line-height: 32px; padding: 5px 0;}
@@ -195,8 +195,8 @@ h2{font-size: 14px; border-bottom: #DADCDF 1px solid; color: #999;}
 
 <body>
 	<span class="lang"><a href="?lang=en-us">English</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="?lang=zh-cn">简体中文</a></span>
-	<h1>WebMIS</h1>
 	<div class="body">
+		<h1>WebMIS</h1>
 <?php if($isWrite){?>
 		<div class="ct"><?php echo $isWrite;?></div>
 <?php }elseif(!$suc){?>
@@ -234,9 +234,9 @@ h2{font-size: 14px; border-bottom: #DADCDF 1px solid; color: #999;}
 		<div class="err" style="text-align: center;"><?php echo $msg;?></div>
 		</form>
 <?php }else{echo $suc;}?>
-	</div>
-	<div class="copy">
-		Copyright © <a href="http://www.ksphp.com/">WebMIS</a> All Rights Reserved
+		<div class="copy">
+			Copyright © <a href="http://www.ksphp.com/">WebMIS</a> All Rights Reserved
+		</div>
 	</div>
 </body>
 </html>
